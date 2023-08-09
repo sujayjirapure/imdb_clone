44c7a7bba8fcdb4201d38ef80c22fd7867a04c40
sujay <jirapuresujay@gmail.com>
2023-08-09 14:39:04 +0530

    half way

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import Home from "./components/Home";
+import Home from "./pages/home/Home";
 import { BrowserRouter as Router ,Routes, Route } from "react-router-dom";
 
 import './App.css';
@@ -12,6 +12,8 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="movies/:id" element={<h1>movie id</h1>}/>
+          <Route path="movies/:type" element={<h1>movies type</h1>}/>
           <Route path="/*" element={<h1>error page not found</h1>}/>
         </Routes>
       </Router>
--- src/components/Header.js
+++ src/components/Header.js
@@ -7,9 +7,9 @@ function Header() {
     <div className="header">
         <div className="headerLeft">
             <Link to="/"><img className="header_icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/575px-IMDB_Logo_2016.svg.png" alt="img"/></Link>
-            <Link to="/movies/popular" style={{textDecoration: "none"}}><span>popular</span></Link>
-            <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>top rated</span></Link>
-            <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>upcoming</span></Link>
+            <Link to="/movies/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+            <Link to="/movies/top_rated" style={{textDecoration: "none"}}><span>Top rated</span></Link>
+            <Link to="/movies/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
         </div>
     </div>
   );
